b6b67cf0fe4d1a51241a33e2c305b64de13bdb45
Kitagarin <minecrafter55571@gmail.com>
2026-06-07 16:05:48 +0300

    New file

---
+++ gosha/While and Input data.py
@@ -0,0 +1,52 @@
+# начал тему 07.06.26(сегодня в америке 06.07.26 типа сиксевен)
+
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+# name = input("Enter your name: ")
+# print(f"Hello {name.title()}")
+
+# prompt = "If you tell us who are you, we can personalize tha messages that you will see.\nWhat is your name?\n"
+
+#ИЛИ
+
+# prompt = "If you tell us who are you, we can personalize tha messages that you will see.\n"
+# prompt += "What is your name?\n"
+
+# name = input(prompt)
+# print(f"Hello, {name.title()}")
+
+# height = input("How tall are you, in centimeteres?\n")
+# height = int(height)
+# if height >= 150:
+# 	print("\nYou are tall enough to ride")
+# else:
+# 	print("\nYou are not tall enough to ride")
+
+# num = input("Enter a number, and I will tell you if it's even or odd:\n")
+# num = int(num)
+# if num % 2 == 0:
+# 	print(f"The number {num} is even")
+# else:
+# 	print(f"The number {num} is odd")
+
+# упражнения
+
+# car = input("What car you like:\n")
+# print(f"I will find car like a {car}")
+
+# num_of_ppl = input("How much people will come with you?\n")
+# num_of_ppl = int(num_of_ppl)
+# if num_of_ppl > 8:
+# 	print(f"{num_of_ppl} people too much, please wait")
+# else:
+# 	print(f"We have table for {num_of_ppl} people")
+
+# num10 = input("Write number, and I tell you if it's even or odd:\n")
+# num10 = int(num10)
+# if num10 % 10 == 0:
+# 	print(f"Number {num10} is divisible by 10")
+# else: 
+# 	print(f"Number {num10} is not divisible by 10")
+
+# продолжил 08.06.26
