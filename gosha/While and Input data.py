c2403f0f6e0b1593a5d76a678efcbbbd3785deab
Kitagarin <minecrafter55571@gmail.com>
2026-06-08 16:46:53 +0300

    Update

--- gosha/While and Input data.py
+++ gosha/While and Input data.py
@@ -50,3 +50,71 @@
 # 	print(f"Number {num10} is not divisible by 10")
 
 # продолжил 08.06.26
+
+current_num = 1
+while current_num <= 5:
+	print(current_num)
+	current_num += 1
+
+prompt = "\nEnter the name of city, that you want visit: " + "\nEnter 'quit' to end the program. "
+# message = ""
+# while message != 'quit':
+# 	message = input(prompt)
+# 	if message != 'quit':
+# 		print(message)
+
+# active = True
+# while active:
+# 	message = input(prompt)
+# 	if message == 'quit':
+# 		active = False
+# 	else:
+# 		print(message)
+
+# while True:
+# 	city = input(prompt)
+# 	if city == 'quit':
+# 		break
+# 	else:
+# 		print(f"I'd to go to {city.title()}")
+
+cur_num = 0
+while cur_num < 10:
+	cur_num += 1
+	if cur_num % 2 == 0:
+		continue
+	print(cur_num)
+
+x = 1
+while x <= 5:
+	print(x)
+	x += 1
+
+# упражнения 
+
+# pizza = "\nWrite name of pizza, that you would like to order: "
+# while True:
+# 	mes = input(pizza)
+# 	if mes == 'quit':
+# 		break
+# 	else:
+# 		print(mes)
+
+age = input("Write your age: ")
+age = int(age)
+while True:
+	if age < 3:
+		cost = "$0"
+		print(f"Your ticket cost is {cost}")
+		break
+	elif age >= 3 and age < 12:
+		cost = "$10"
+		print(f"Your ticket cost is {cost}")
+		break
+	elif age >= 12:
+		cost = "$15"
+		print(f"Your ticket cost is {cost}") 
+		break
+	
+		
+
