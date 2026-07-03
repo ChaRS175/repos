9eb73e85b116c11133de37c518c94a82c255bcd1
Kitagarin <minecrafter55571@gmail.com>
2026-07-03 16:38:16 +0300

    Aliens

--- gosha/Projects/Alien_Invasion/settings.py
+++ gosha/Projects/Alien_Invasion/settings.py
@@ -4,7 +4,7 @@ class Settings():
 		self.screen_width = 800
 		self.screen_height = 500
 		self.bg_color = (100, 150, 200)
-		self.ship_speed = 5.5
+		self.ship_speed = 3.5
 
 		self.bullet_speed = 3
 		self.bullet_width = 10
