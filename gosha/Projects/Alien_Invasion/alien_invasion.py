7ecf8eb0ae436810834c822a9b0d2548a55d5734
Kitagarin <minecrafter55571@gmail.com>
2026-07-04 16:39:21 +0300

    Stars and moving

--- gosha/Projects/Alien_Invasion/alien_invasion.py
+++ gosha/Projects/Alien_Invasion/alien_invasion.py
@@ -1,13 +1,14 @@
 # начал вроде 26.06.26
 
 import sys
-
+from random import randint
 import pygame
 
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from star import Star
 
 class AlienInvasion:
 
@@ -24,20 +25,18 @@ class AlienInvasion:
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
 		self.aliens = pygame.sprite.Group()
+		self.stars = pygame.sprite.Group()
 
+		self._create_row_stars()
 		self._create_fleet()
+
 
 	def run_game(self):
 		while True: # отслеживание событий клавиатуры и мыши
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
-
-			for bullet in self.bullets.copy():
-				if bullet.rect.bottom <= 0:
-					self.bullets.remove(bullet)
-			print(len(self.bullets))
-			
+			self._bullets_update()
+			self._update_aliens()
 			self._update_screen()
 
 
@@ -71,6 +70,15 @@ class AlienInvasion:
 			new_bullet = Bullet(self)
 			self.bullets.add(new_bullet)
 
+	def _bullets_update(self):
+
+		self.bullets.update()
+
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+		# print(len(self.bullets))
+
 	def _create_fleet(self):
 		alien = Alien(self)
 		alien_width, alien_height = alien.rect.size
@@ -92,13 +100,55 @@ class AlienInvasion:
 		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_num
 		self.aliens.add(alien)
 
+	def _check_fleet_edges(self):
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
+
+	def _update_aliens(self):
+		self._check_fleet_edges()
+		self.aliens.update()
+
+	def _create_row_stars(self):
+		star = Star(self)
+		star_width, star_height = star.rect.size
+		available_space_x = self.settings.screen_width - (star_width // 2)
+		num_stars_x = available_space_x // (2 * star_width)
+
+		ship_height = self.ship.rect.height
+		available_space_y = self.settings.screen_height - (star_height // 2) - ship_height
+		num_rows_star = available_space_y // (2 * star_height)
+		for row_star_num in range(num_rows_star):
+			for star_num in range(num_stars_x):
+				self._create_star(star_num, row_star_num)
+
+	def _create_star(self, star_num, row_star_num):
+		star = Star(self)
+		star_width, star_height = star.rect.size
+
+		random_move_x = randint(-100, 100)
+		random_move_y = randint(-100, 100)
+
+		star.x = star_width + 2 * star_width * star_num + random_move_x
+		star.rect.x = star.x
+		star.rect.y = star.rect.height + 2 * star.rect.height * row_star_num + random_move_y
+		self.stars.add(star)
+
 	def _update_screen(self):
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 
+		self.stars.draw(self.screen)
 		self.aliens.draw(self.screen)
+
 		# отображение последнего прорисованного экрана
 		pygame.display.flip()
 
--- gosha/Projects/Alien_Invasion/settings.py
+++ gosha/Projects/Alien_Invasion/settings.py
@@ -5,9 +5,13 @@ class Settings():
 		self.screen_height = 500
 		self.bg_color = (100, 150, 200)
 		self.ship_speed = 3.5
+		self.alien_speed = 1
+		self.fleet_drop_speed = 10
+		# fleet_direction = 1 это движение вправо, а -1 движение влево
+		self.fleet_direction = 1
 
 		self.bullet_speed = 3
 		self.bullet_width = 10
 		self.bullet_height = 30
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 10
